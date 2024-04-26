e572eef92e048823aafbaae89c5ff58c1bd61324
leoworqu <132668626+leoworqu@users.noreply.github.com>
2024-04-26 04:14:47 +0300

    Create 3-currency.js

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,31 @@
+export default class Currency {
+    constructor(code, name) {
+        this._code = this._validateString(code, "code");
+        this._name = this._validateString(name, "name");
+    }
+
+    get code() {
+        return this._code;
+    }
+    set code(value) {
+        this._code = this._validateString(value, "code");
+    }
+
+    get name() {
+        return this._name;
+    }
+    set name(value) {
+        this._name = this._validateString(value, "name");
+    }
+
+    displayFullCurrency() {
+        return `${this._name} (${this._code})`;
+    }
+
+    _validateString(value, attributeName) {
+        if (typeof value !== "string") {
+            throw new TypeError(`${attributeName} must be a string.`);
+        }
+        return value;
+    }
+}
